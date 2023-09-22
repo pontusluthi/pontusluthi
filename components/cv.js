0addb0e11bf0fc0e1d739d814327bf74dc33f242
Pontus Lüthi <pontus.luthi@gmail.com>
2023-09-22 13:53:58 +0200

    did stuff

---
+++ components/cv.js
@@ -0,0 +1,7 @@
+export default function Cv({className}) {
+    return (
+        <div className={className}>
+            <p>test</p>
+        </div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,11 @@ import React, {useEffect, useState} from "react";
 import styles from "../styles/Home.module.css";
 import '../components/balls'
 import Balls from "../components/balls";
+import Cv from "../components/cv"
 
 export default function Home() {
     const [field, setField] = useState("circle");
-
+    const [showCv, setShowCv] = useState(false);
 
     return (
         <div className={styles.body}>
@@ -16,15 +17,22 @@ export default function Home() {
 
             <Balls key={field} field={field}/>
 
-            <div className={styles.name}>
+            <button className={styles.name} onClick={() => {
+                setShowCv(showCv => !showCv);
+                setField(field === "circle" ? "disperse" : "circle");
+            }}>
                 <h1>Pontus Lüthi</h1>
-            </div>
+            </button>
 
             <div className={styles.controls}>
                 <button onClick={() => setField("circle")}> Circle </button>
                 <button onClick={() => setField("disperse")}> Disperse </button>
                 <button onClick={() => setField("lump")}> Lump </button>
             </div>
+
+
+            <Cv className={showCv ? styles.cv : `${styles.cv} ${styles.hideCv}`} />
+
         </div>
     );
 }
